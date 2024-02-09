c36a9fb606fd07de42103a80aad10e824b62703e
SwathiKSuresh <swathiksuresh25@gmail.com>
2024-02-09 12:00:31 +0530

    new sign-in

--- Controllers/SignUp.js
+++ Controllers/SignUp.js
@@ -2,13 +2,29 @@ const express=require("express")
 const userModel=require("../Models/userModel")
 const router=express.Router()
 
-router.post("/signup",async(req,res)=>{
-    let data=req.body
-    let user=new userModel(data)
-    let result=await user.save()
-    res.json({
-        status:"success"
-    })
-})
+const bcrypt=require("bcryptjs")
 
+hashPasswordGenerator=async(pass)=>{
+    const salt=await bcrypt.genSalt(10)
+    return bcrypt.hash(pass,salt)
+}
+
+router.post("/signup",async(req,res)=>{
+    let {data}={"data":req.body}
+    let password=data.password
+    hashPasswordGenerator(password).then(
+        (hashedPassword)=>{
+            console.log(hashedPassword)
+            data.password=hashedPassword
+            console.log(data)
+            let user=new userModel(data)
+            let result=user.save()
+            res.json({
+                status:"success"
+            })
+        }
+    )
+   
+   
+})
 module.exports=router
