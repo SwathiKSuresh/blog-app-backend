2fbc14c23e9d4715ef8565027771272f433f621f
SwathiKSuresh <swathiksuresh25@gmail.com>
2024-02-09 12:40:38 +0530

    Add email and password Authentication

--- Controllers/SignUp.js
+++ Controllers/SignUp.js
@@ -24,7 +24,34 @@ router.post("/signup",async(req,res)=>{
             })
         }
     )
-   
-   
 })
+
+router.post("/signin",async(req,res)=>{
+    let input=req.body
+    let email=req.body.email
+    let data=await userModel.findOne({"email":email})
+    if (!data) {
+        return res.json({
+            status:"invalid user entry"
+        })
+        
+    }
+    console.log(data)
+    let dbPassword=data.password
+    let inputPassword=req.body.password
+    console.log(dbPassword)
+    console.log(inputPassword)
+
+    const match=await bcrypt.compare(inputPassword,dbPassword)
+    if (!match) {
+        return res.json({
+            status:"Invalid password"
+        })
+    }
+
+    res.json({
+        status:"success"
+    })
+})
+
 module.exports=router
